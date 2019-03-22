e9432af7d1239643750cdba783a45d8143d077e2
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2019-03-22 15:37:17 +0000

    make it a package

---
+++ piccolo3/client/__init__.py
@@ -0,0 +1,29 @@
+# Copyright 2014-2016 The Piccolo Team
+#
+# This file is part of piccolo3-server.
+#
+# piccolo3-server is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# piccolo3-server is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with piccolo3-server.  If not, see <http://www.gnu.org/licenses/>.
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution('piccolo3-server').version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+from .Piccolo import *
+from .PiccoloDataDir import *
+from .PiccoloSysinfo import *
+from .PiccoloSpectrometer import *
