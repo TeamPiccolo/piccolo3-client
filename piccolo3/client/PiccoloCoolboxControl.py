e7726a7a109b5510ba0b6a6960becb8cffe91469
Murray Hoggett <murrayhoggett@gmail.com>
2020-11-21 15:02:54 +0000

    Added PiccoloVoltage proxy class

--- piccolo3/client/PiccoloCoolboxControl.py
+++ piccolo3/client/PiccoloCoolboxControl.py
@@ -25,14 +25,15 @@ __all__ = ['PiccoloCoolboxControl']
 from .PiccoloBaseClient import *
 import asyncio
 
+
 class PiccoloTemperature(PiccoloNamedClientComponent):
     """manage temperature control on coolbox"""
 
     NAME = "coolboxctrl"
 
-    def __init__(self,baseurl,name):
-        
-        super().__init__(baseurl,name)
+    def __init__(self, baseurl, name):
+
+        super().__init__(baseurl, name)
 
         self._target_temp = None
         self._current_temp = None
@@ -42,27 +43,30 @@ class PiccoloTemperature(PiccoloNamedClientComponent):
         self.add_task(self._get_target_temp())
         self.add_task(self._get_current_temp())
 
-    def register_callback(self,cb):
+    def register_callback(self, cb):
         self._callbacks.append(cb)
 
     async def _get_target_temp(self):
         u = 'target_temp'
         async for s in self.a_observe(u):
             for cb in self._callbacks:
-                await cb(json.dumps((self.name,u,s)))
+                await cb(json.dumps((self.name, u, s)))
             self._target_temp = s
+
     async def get_target_temp(self):
         self._targetTemp = await self.a_get('target_temp')
         return self._targetTemp
-    async def set_target_temp(self,t):
-        await self.a_put('target_temp',float(t))
+
+    async def set_target_temp(self, t):
+        await self.a_put('target_temp', float(t))
 
     async def _get_current_temp(self):
         u = 'current_temp'
         async for s in self.a_observe(u):
             for cb in self._callbacks:
-                await cb(json.dumps((self.name,u,s)))
+                await cb(json.dumps((self.name, u, s)))
             self._current_temp = s
+
     async def get_current_temp(self):
         self.log.info(self.uri('current_temp'))
         self._current_temp = await self.a_get('current_temp')
@@ -71,16 +75,53 @@ class PiccoloTemperature(PiccoloNamedClientComponent):
     @property
     def target_temp(self):
         return self._target_temp
+
     @property
     def current_temp(self):
         return self._current_temp
+
+
+class PiccoloVoltage(PiccoloNamedClientComponent):
+    """manage voltage control on coolbox"""
+
+    NAME = "coolboxctrl"
+
+    def __init__(self, baseurl, name):
+
+        super().__init__(baseurl, name)
+
+        self._current_voltage = None
+
+        self._callbacks = []
+
+        self.add_task(self._get_current_voltage())
+
+    def register_callback(self, cb):
+        self._callbacks.append(cb)
+
+    async def _get_current_voltage(self):
+        u = 'current_voltage'
+        async for s in self.a_observe(u):
+            for cb in self._callbacks:
+                await cb(json.dumps((self.name, u, s)))
+            self._current_voltage = s
+
+    async def get_current_voltage(self):
+        self.log.info(self.uri('current_voltage'))
+        self._current_voltage = await self.a_get('current_voltage')
+        return self._current_voltage
+
+    @property
+    def current_voltage(self):
+        return self._current_voltage
+
 
 class PiccoloCoolboxControl(PiccoloClientComponent):
     """manage temperature control on coolbox"""
 
     NAME = "coolboxctrl"
 
-    def __init__(self,baseurl):
+    def __init__(self, baseurl):
         super().__init__(baseurl)
 
         self._temperature_sensors = {}
@@ -93,11 +134,12 @@ class PiccoloCoolboxControl(PiccoloClientComponent):
         sensors = await self.get_temperature_sensors()
         for t in sensors:
             if t not in self.temperature_sensors:
-                self.temperature_sensors[t] = PiccoloTemperature(self.baseurl,t)
+                self.temperature_sensors[t] = PiccoloTemperature(
+                    self.baseurl, t)
                 for c in self._callbacks:
                     self.temperature_sensors[t].register_callback(c)
 
-    def register_callback(self,cb):
+    def register_callback(self, cb):
         self._callbacks.append(cb)
         for t in self.temperature_sensors:
             self.temperature_sensors[t].register_callback(cb)
@@ -106,7 +148,7 @@ class PiccoloCoolboxControl(PiccoloClientComponent):
         for t in self.temperature_sensors:
             self.temperature_sensors[t].shutdown_tasks()
         super().shutdown_tasks()
-            
+
     async def get_temperature_sensors(self):
         if self._temps is None:
             self._temps = await self.a_get('temperature_sensors')
@@ -116,20 +158,21 @@ class PiccoloCoolboxControl(PiccoloClientComponent):
     def temperature_sensors(self):
         return self._temperature_sensors
 
+
 async def main():
     base = 'coap://localhost'
 
     coolbox = PiccoloCoolboxControl(base)
     await asyncio.sleep(1)
 
-    print (coolbox.temperature_sensors.keys())
+    print(coolbox.temperature_sensors.keys())
     for i in range(10):
         for temp in coolbox.temperature_sensors:
-            print (temp,coolbox.temperature_sensors[temp].current_temp)
+            print(temp, coolbox.temperature_sensors[temp].current_temp)
         await asyncio.sleep(1)
     coolbox.shutdown_tasks()
     await asyncio.sleep(5)
-    
+
 if __name__ == '__main__':
     from piccolo3.common import piccoloLogging
     piccoloLogging(debug=True)
